133c29f4c1be5cd81145201f053d643f188b190c
emotionalcenter <contacto.amistapp@gmail.com>
2025-12-09 20:39:44 -0300

    Actualización de sesión persistente

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./router";
-import App from "./App";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App>
-      <AppRouter />
-    </App>
+    <AppRouter />
   </React.StrictMode>
 );
