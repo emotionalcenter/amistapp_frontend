03cad01e66fdc1eff9117f69c962c596bd779bef
emotionalcenter <contacto.amistapp@gmail.com>
2025-12-09 15:11:04 -0300

    Fix: App wrapped around AppRouter for auth context

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./router";
+import App from "./App";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppRouter />
+    <App>
+      <AppRouter />
+    </App>
   </React.StrictMode>
 );
